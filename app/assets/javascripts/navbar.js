f93027fb3ed8cb60ba4f64c74968e1e1ccc03aaa
Marcelo Bentivoglio <marcelo.bentivoglio@mvpinvest.com.br>
2017-12-21 10:45:40 -0200

    castly-prep workflow

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,14 @@
+function navScroll() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    document.getElementById("nav").classList.remove("hide");
+  } else {
+    document.getElementById("nav").classList.add("hide");
+  }
+}
+
+window.onscroll = function() {
+  navScroll();
+};
